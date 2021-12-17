047d7c65cbd580aab4c7f00509f657efa135807a
David8393 <jo974586david@gmail.com>
2021-12-17 21:10:15 +0500

    Masala

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
-``
+// var a = 1,
+//     b = 3
+// console.log(a + b);
+
+// const data = () => {
+//     var a = 1;
+//     var b = 4;
+//     console.log();
+
+// }
+    
+// console.log(data());
+
+// var str = '123456789'
+// function data() {
+       
+// }
+
+// const func = (num) => {
+//     var temp = num;
+//     var reversed = String(num).split("").reverse().join("");
+//     return parseInt(`${temp} ${reversed}`);
+// }
+// console.log(func(123456));
+// console.log(func(152));
+// console.log(func(123456789));
+// 1-masala
+// function data(letter) {
+//    return letter === letter.toUpperCase()
+    
+// }
+// console.log(data('a'));
+// data = a => a === a.toUpperCase()
+// console.log(data('f'));
+
+// function data(letter) {
+//     console.log(letter=== letter.toUpperCase());
+    
+// }
+// data ('d')
+
+// function data(letter) {
+//   return letter.includes('a')
+// }
+// console.log(data('Webbrain academy'));
+
+// var data = 'Webbrain Academy'
+// var a = 0
+// var b = 0
+// for (let i = 0; i < data.length; i++) {
+//     (data[i] == 'a' || data[i] == 'A') && a++
+// }
+// console.log(a);
+
+// 2-masala
+// letter = 'Webbrain Academy';
+// console.log( letter.match(/a/gi).length)
+
+// var a = '123456789'
+// var b = parseInt(a)
+// console.log(a.match(/g).length);
+
+// var Otabek = 'lorem / lorem *'
+// Shaxzod = Otabek.split('*').join('')
+// console.log(Shaxzod.split('/').join(''));
+// 4-masala
+// function data(letter) {
+//     var letter1 = letter.split('*').join('');
+//     return letter1.split('/').join('')
+// }
+// console.log(data('lorem / lorem *'));
+// console.log(typeof 2);
+
+////////////////////////////////////////
---
+++ index1.js
@@ -0,0 +1,31 @@
+console.log('Otabek') // berilayotgan buyrug'imizni yechimi
+
+// datatypes
+// Number, string
+
+
+// var , let ,const
+// let a = 2
+// let b = 3
+// let c = a+b
+// console.log(c);
+
+// + , / , *, -
+// var a = 10
+// var b = 10
+// var b = 10 + 5
+// console.log(a+b);
+
+// +=, -=, qoshish va ayirish uchun qisqa usul
+// var a = 10
+// var b = 10
+// b += 5
+// a -= 5
+// a = b
+// b = a
+
+// console.log(a+b);
+
+// console.log('', 100);
+
+// '', "", `` bularni ichida yozilgan har bir ma'lumot bizga string javob qaytaradi
