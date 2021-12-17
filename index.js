c4038754dece3445f70144eac779b29d8701733b
David8393 <jo974586david@gmail.com>
2021-12-17 22:50:10 +0500

    Masala

--- index.js
+++ index.js
@@ -24,26 +24,30 @@
 // console.log(func(123456));
 // console.log(func(152));
 // console.log(func(123456789));
-
-// 1-masala
+///////////////////////////////////////////////////
+// Masalalar//
+// 1-masala 1-Holat
 // function data(letter) {
 //    return letter === letter.toUpperCase()
 // }
 // console.log(data('a'));
 
+// 2-Holat
 // data = a => a === a.toUpperCase()
 // console.log(data('f'));
 
+// 3-Holat
 // function data(letter) {
 //     console.log(letter=== letter.toUpperCase());
-    
 // }
 // data ('d')
 
-// function data(letter) {
-//   return letter.includes('a')
-// }
-// console.log(data('Webbrain academy'));
+
+// 2-masala 1-Holat
+// letter = 'Webbrain Academy';
+// console.log( letter.match(/a/gi).length)
+
+// 2-Holat
 
 // var data = 'Webbrain Academy'
 // var a = 0
@@ -52,23 +56,26 @@
 // }
 // console.log(a);
 
-// 2-masala
-// letter = 'Webbrain Academy';
-// console.log( letter.match(/a/gi).length)
-
-
-// var Otabek = 'lorem / lorem *'
-// Shaxzod = Otabek.split('*').join('')
-// console.log(Shaxzod.split('/').join(''));
 // 4-masala
+// 1-Holat
 // function data(letter) {
     //     var letter1 = letter.split('*').join('');
     //     return letter1.split('/').join('')
     // }
     // console.log(data('lorem / lorem *'));
-    // console.log(typeof 2);
-    
+// 2-Holat
+    // var Otabek = 'lorem / lorem *'
+    // Shaxzod = Otabek.split('*').join('')
+    // console.log(Shaxzod.split('/').join(''));
+// 3-Holat
+    // function data(letter) {
+    //     var letter1 = letter.replace('*', '');
+    //     return letter1.replace('/', '')
+    // }
+    // console.log(data('lorem / lorem *'));
+
     ////////////////////////////////////////
+    
     //  var a = '123456789';
      // var b = parseInt(a)
      // console.log(a.match(/g).length);
