342a7dbe2b767d71237c1f28accfe0da80da837e
David8393 <jo974586david@gmail.com>
2021-12-18 14:21:04 +0500

    Masala

--- index.js
+++ index.js
@@ -8,12 +8,12 @@
 //     console.log();
 
 // }
-    
+
 // console.log(data());
 
 // var str = '123456789'
 // function data() {
-       
+
 // }
 
 // const func = (num) => {
@@ -25,6 +25,7 @@
 // console.log(func(152));
 // console.log(func(123456789));
 ///////////////////////////////////////////////////
+
 // Masalalar//
 // 1-masala 1-Holat
 // function data(letter) {
@@ -42,7 +43,6 @@
 // }
 // data ('d')
 
-
 // 2-masala 1-Holat
 // letter = 'Webbrain Academy';
 // console.log( letter.match(/a/gi).length)
@@ -56,27 +56,58 @@
 // }
 // console.log(a);
 
+// 3-masala 1-Holat
+// var num = '123456789';
+// var a = 0;
+
+// b = num.split('');
+
+// b.map((value) => {
+//   a += parseInt(value);
+// });
+// console.log(a);
+
+// 2-Holat
+// var num = '123456789';
+
+// var a = 0;
+
+// for (i = 0; i <= num.length; i+2) {
+//   a += i;
+// }
+// console.log(a);
+
+// 3-Holat
+
+// function sumOfStr(str) {
+//   var a = num.split('');
+//   return a.reduce(function (first, second) {
+//     return parseInt(first) + parseInt(second);
+//   });
+// }
+// console.log(sumOfStr('123456789'));
 // 4-masala
 // 1-Holat
 // function data(letter) {
-    //     var letter1 = letter.split('*').join('');
-    //     return letter1.split('/').join('')
-    // }
-    // console.log(data('lorem / lorem *'));
+//     var letter1 = letter.split('*').join('');
+//     return letter1.split('/').join('')
+// }
+// console.log(data('lorem / lorem *'));
 // 2-Holat
-    // var Otabek = 'lorem / lorem *'
-    // Shaxzod = Otabek.split('*').join('')
-    // console.log(Shaxzod.split('/').join(''));
+// function data(letter) {
+//     var letter1 = letter.replace('*', '');
+//     return letter1.replace('/', '')
+// }
+// console.log(data('lorem / lorem *'));
 // 3-Holat
-    // function data(letter) {
-    //     var letter1 = letter.replace('*', '');
-    //     return letter1.replace('/', '')
-    // }
-    // console.log(data('lorem / lorem *'));
-
-    ////////////////////////////////////////
-    
-    //  var a = '123456789';
-     // var b = parseInt(a)
-     // console.log(a.match(/g).length);
-     // console.log(a.length * a[4]);
+// var Otabek = 'lorem / lorem *'
+// Shaxzod = Otabek.split('*').join('')
+// console.log(Shaxzod.split('/').join(''));
+//4-Holat
+// function data(str) {
+//   console.log(str.match(/\w/g).join(''));
+// }
+// data(`Webbrain // Academy ** `);
+////////////////////////////////////////
+// var a = 'gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg'
+// console.log(a.length);
